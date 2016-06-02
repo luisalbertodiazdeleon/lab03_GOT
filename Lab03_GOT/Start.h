dc54c5e3ddf4358b958b3fdc88f78434119e4b41
Luis Diaz <luisdiaz@luiss-imac.local>
2016-06-01 19:16:29 -0500

    all buttons working, ready for new view controller

--- Lab03_GOT/Start.h
+++ Lab03_GOT/Start.h
@@ -15,7 +15,77 @@
 @property (weak, nonatomic) IBOutlet UIButton *Live1_Info;
 - (IBAction)Dead1:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet UIButton *Dead1_Info;
-
 @property (weak, nonatomic) IBOutlet UIImageView *answer1;
+
+- (IBAction)Live2:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Live2_Info;
+- (IBAction)Dead2:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Dead2_Info;
+@property (weak, nonatomic) IBOutlet UIImageView *answer2;
+
+- (IBAction)Live3:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Live3_Info;
+- (IBAction)Dead3:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Dead3_Info;
+@property (weak, nonatomic) IBOutlet UIImageView *answer3;
+
+- (IBAction)Live4:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Live4_Info;
+- (IBAction)Dead4:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Dead4_Info;
+@property (weak, nonatomic) IBOutlet UIImageView *answer4;
+
+- (IBAction)Live5:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Live5_Info;
+- (IBAction)Dead5:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Dead5_Info;
+@property (weak, nonatomic) IBOutlet UIImageView *answer5;
+
+- (IBAction)Live6:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Live6_Info;
+- (IBAction)Dead6:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Dead6_Info;
+@property (weak, nonatomic) IBOutlet UIImageView *answer6;
+
+- (IBAction)Live7:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Live7_Info;
+- (IBAction)Dead7:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Dead7_Info;
+@property (weak, nonatomic) IBOutlet UIImageView *answer7;
+
+- (IBAction)Live8:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Live8_Info;
+- (IBAction)Dead8:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Dead8_Info;
+@property (weak, nonatomic) IBOutlet UIImageView *answer8;
+
+- (IBAction)Live9:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Live9_Info;
+- (IBAction)Dead9:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Dead9_Info;
+@property (weak, nonatomic) IBOutlet UIImageView *answer9;
+
+- (IBAction)Live10:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Live10_Info;
+- (IBAction)Dead10:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *Dead10_Info;
+@property (weak, nonatomic) IBOutlet UIImageView *answer10;
+
+
+
+- (IBAction)next:(UIButton *)sender;
+
+
+
 @end
+
+
+
+
+
+
+
+
+
+
 
--- Lab03_GOT/Start.m
+++ Lab03_GOT/Start.m
@@ -14,6 +14,8 @@
 
 @implementation Start
 
+static int cont = 0;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.scrolly setScrollEnabled:YES];
@@ -28,15 +30,120 @@
 
 - (IBAction)Live1:(UIButton *)sender {
     //NSLog(@"Done");
-    self.Live1_Info.backgroundColor = [UIColor redColor];
+    self.Live1_Info.backgroundColor = [UIColor yellowColor];
     self.answer1.image = [UIImage imageNamed:@"ok.jpg"];
     self.Dead1_Info.enabled = FALSE;
+    cont++;
 }
 
 - (IBAction)Dead1:(UIButton *)sender {
-    self.Dead1_Info.backgroundColor = [UIColor redColor];
+    self.Dead1_Info.backgroundColor = [UIColor yellowColor];
     self.answer1.image = [UIImage imageNamed:@"mal.jpg"];
     self.Live1_Info.enabled = FALSE;
+}
+- (IBAction)Live2:(UIButton *)sender {
+    self.Live2_Info.backgroundColor = [UIColor yellowColor];
+    self.answer2.image = [UIImage imageNamed:@"ok.jpg"];
+    self.Dead2_Info.enabled = FALSE;
+    cont++;
+}
+- (IBAction)Dead2:(UIButton *)sender {
+    self.Dead2_Info.backgroundColor = [UIColor yellowColor];
+    self.answer2.image = [UIImage imageNamed:@"mal.jpg"];
+    self.Live2_Info.enabled = FALSE;
+}
+- (IBAction)Live3:(UIButton *)sender {
+    self.Live3_Info.backgroundColor = [UIColor yellowColor];
+    self.answer3.image = [UIImage imageNamed:@"mal.jpg"];
+    self.Dead3_Info.enabled = FALSE;
+}
+- (IBAction)Dead3:(UIButton *)sender {
+    self.Dead3_Info.backgroundColor = [UIColor yellowColor];
+    self.answer3.image = [UIImage imageNamed:@"ok.jpg"];
+    self.Live3_Info.enabled = FALSE;
+    cont++;
+}
+- (IBAction)Live4:(UIButton *)sender {
+    self.Live4_Info.backgroundColor = [UIColor yellowColor];
+    self.answer4.image = [UIImage imageNamed:@"mal.jpg"];
+    self.Dead4_Info.enabled = FALSE;
+}
+- (IBAction)Dead4:(UIButton *)sender {
+    self.Dead4_Info.backgroundColor = [UIColor yellowColor];
+    self.answer4.image = [UIImage imageNamed:@"ok.jpg"];
+    self.Live4_Info.enabled = FALSE;
+    cont++;
+}
+- (IBAction)Live5:(UIButton *)sender {
+    self.Live5_Info.backgroundColor = [UIColor yellowColor];
+    self.answer5.image = [UIImage imageNamed:@"mal.jpg"];
+    self.Dead5_Info.enabled = FALSE;
+}
+- (IBAction)Dead5:(UIButton *)sender {
+    self.Dead5_Info.backgroundColor = [UIColor yellowColor];
+    self.answer5.image = [UIImage imageNamed:@"ok.jpg"];
+    self.Live5_Info.enabled = FALSE;
+    cont++;
+}
+- (IBAction)Live6:(UIButton *)sender {
+    self.Live6_Info.backgroundColor = [UIColor yellowColor];
+    self.answer6.image = [UIImage imageNamed:@"ok.jpg"];
+    self.Dead6_Info.enabled = FALSE;
+    cont++;
+}
+- (IBAction)Dead6:(UIButton *)sender {
+    self.Dead6_Info.backgroundColor = [UIColor yellowColor];
+    self.answer6.image = [UIImage imageNamed:@"mal.jpg"];
+    self.Live6_Info.enabled = FALSE;
+}
+- (IBAction)Live7:(UIButton *)sender {
+    self.Live7_Info.backgroundColor = [UIColor yellowColor];
+    self.answer7.image = [UIImage imageNamed:@"ok.jpg"];
+    self.Dead7_Info.enabled = FALSE;
+    cont++;
+}
+- (IBAction)Dead7:(UIButton *)sender {
+    self.Dead7_Info.backgroundColor = [UIColor yellowColor];
+    self.answer7.image = [UIImage imageNamed:@"mal.jpg"];
+    self.Live7_Info.enabled = FALSE;
+}
+- (IBAction)Live8:(UIButton *)sender {
+    self.Live8_Info.backgroundColor = [UIColor yellowColor];
+    self.answer8.image = [UIImage imageNamed:@"mal.jpg"];
+    self.Dead8_Info.enabled = FALSE;
+}
+- (IBAction)Dead8:(id)sender {
+    self.Dead8_Info.backgroundColor = [UIColor yellowColor];
+    self.answer8.image = [UIImage imageNamed:@"ok.jpg"];
+    self.Live8_Info.enabled = FALSE;
+    cont++;
+}
+- (IBAction)Live9:(UIButton *)sender {
+    self.Live9_Info.backgroundColor = [UIColor yellowColor];
+    self.answer9.image = [UIImage imageNamed:@"mal.jpg"];
+    self.Dead9_Info.enabled = FALSE;
+}
+- (IBAction)Dead9:(UIButton *)sender {
+    self.Dead9_Info.backgroundColor = [UIColor yellowColor];
+    self.answer9.image = [UIImage imageNamed:@"ok.jpg"];
+    self.Live9_Info.enabled = FALSE;
+    cont++;
+}
+- (IBAction)Live10:(UIButton *)sender {
+    self.Live10_Info.backgroundColor = [UIColor yellowColor];
+    self.answer10.image = [UIImage imageNamed:@"ok.jpg"];
+    self.Dead10_Info.enabled = FALSE;
+    cont++;
+}
+- (IBAction)Dead10:(UIButton *)sender {
+    self.Dead10_Info.backgroundColor = [UIColor yellowColor];
+    self.answer10.image = [UIImage imageNamed:@"mal.jpg"];
+    self.Live10_Info.enabled = FALSE;
+}
+- (IBAction)next:(UIButton *)sender {
+    
+    NSLog(@"Done %d",cont);
+    
 }
 @end
 
